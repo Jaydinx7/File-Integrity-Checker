e513b6785c9a3ae054b622aecf3938d08419875e
Jaydin Solomons <jasolcpt025@student.wethinkcode.co.za>
2026-06-30 21:18:42 +0200

    feat: implement user input and file path validation. Closes #2

--- src/main/java/org/example/IntegrityChecker.java
+++ src/main/java/org/example/IntegrityChecker.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class IntegrityChecker {
 
     static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the path to the file you want to check: ");
+        String filePath = scanner.nextLine();
 
+       try{
+           System.out.println("File path recieved: " + filePath);
+       } catch (Exception e) {
+           System.out.println("ERROR: Something went wrong.");
+       }
+
+       scanner.close();
     }
 }
